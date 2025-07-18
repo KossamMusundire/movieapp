e591dd208241a286a429877d5a10899612ad0a18
Kossam Musundire <msnkos001@myuct.ac.za>
2025-07-18 17:48:20 +0200

    feat: enhance explore link with button component for improved UI

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text,StyleSheet ,ImageBackground } from 'react-native'
+import { View, Text,StyleSheet ,ImageBackground ,Pressable} from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'  
 
@@ -11,7 +11,15 @@ const app = () => {
     <ImageBackground source={icedCoffeeImg}
     resizeMode='cover' style={styles.image}>
       <Text style ={styles.title}>Coffee Shop</Text>
-      <Link href="/explore" style={styles.link} >Explore</Link>
+
+      <Link href="/explore" style={{marginHorizontal:`auto`}} 
+      asChild>
+        <Pressable style={styles.button}>
+          <Text styles ={styles.buttonText}>Explore</Text></Pressable>
+      
+        </Link>
+
+
       </ImageBackground>
     </View>
   )
@@ -45,6 +53,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     textDecorationLine: 'underline',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 4,
+  },
+  button:{
+    height: 60,
+    borderRadius: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    padding: 6,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight:'bold',
+    textAlign: 'center',
+   
     padding: 4,
   }
 
