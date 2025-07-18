5a5dca4f0134c5ecf53e986b6f2fb679c879a4ec
Kossam Musundire <msnkos001@myuct.ac.za>
2025-07-18 19:21:53 +0200

    refactor: update import path for Colors and restore Colors.ts file

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -5,8 +5,7 @@
 
 import { useColorScheme } from 'react-native';
 
-import { Colors } from '@/constants/Colors';
-
+import { Colors } from '../constants/Colors';
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
