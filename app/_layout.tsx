ff5aaaeffc2f14ae495233fd6cf04cba442330b4
Kossam Musundire <msnkos001@myuct.ac.za>
2025-07-21 11:05:54 +0200

    feat: implement Coffee layout and navigation; add TabBarIcon component and related screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,14 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '../hooks/useColorScheme';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default function RootLayout({children}: PropsWithChildren) {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -28,8 +28,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="(coffee)" options={{ headerShown: false }}/>
         <Stack.Screen name="index"options={ {title:"Home",headerShown:false }} />
         <Stack.Screen name="contact"options={ {title:"Contact Us" }} />
+        <Stack.Screen name ="+not-found"/>
       </Stack>
     </ThemeProvider>
   );
