4b75f30ab801b1d2a89beecdc10162f6c7c2a3f4
Kossam Musundire <msnkos001@myuct.ac.za>
2025-07-21 14:58:34 +0200

    feat: refactor layout and navigation; remove unused components and add Contact screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { PropsWithChildren, useEffect } from 'react';
-import 'react-native-reanimated';
+import { Appearance } from 'react-native';
 
-import { useColorScheme } from '../hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout({children}: PropsWithChildren) {
-  const colorScheme = useColorScheme();
+  const colorScheme = Appearance.getColorScheme()
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;  
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -26,12 +27,13 @@ export default function RootLayout({children}: PropsWithChildren) {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+    
+      <Stack screenOptions={{ headerStyle: { backgroundColor: theme.background }, headerTintColor: theme.text, headerShadowVisible:false }}>
+        <Stack.Screen name="index" options={{ headerShown: false , title:`Home` }}/>
+       < Stack.Screen name="Contact" options={{ headerShown: true , title:`ContactUs` }}/>
 
         <Stack.Screen name ="+not-found"/>
       </Stack>
-    </ThemeProvider>
+    
   );
 }
