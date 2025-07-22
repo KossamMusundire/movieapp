35db31572ee064ceed438e366f72f6f441fefa15
Kossam Musundire <msnkos001@myuct.ac.za>
2025-07-22 17:14:26 +0200

    fix: correct style prop in Contact and Menu buttons

--- app/index.jsx
+++ app/index.jsx
@@ -10,12 +10,20 @@ const app = () => {
     <View style={styles.container}>
     <ImageBackground source={icedCoffeeImg}
     resizeMode='cover' style={styles.image}>
+
       <Text style ={styles.title}>Coffee Shop</Text>
 
       <Link href="/contact" style={{marginHorizontal: 'auto'}} 
       asChild>
         <Pressable style={styles.button}>
-          <Text styles ={styles.buttonText}>Contact Us</Text></Pressable>
+          <Text style ={styles.buttonText}>Contact Us</Text></Pressable>
+      
+        </Link>
+
+      <Link href="/menu" style={{marginHorizontal: 'auto'}} 
+      asChild>
+        <Pressable style={styles.button}>
+          <Text style ={styles.buttonText}>Menu</Text></Pressable>
       
         </Link>
 
